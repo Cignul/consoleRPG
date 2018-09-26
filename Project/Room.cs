2220179893f85caad40ba38c546ffead41d30613
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-09-26 16:49:29 -0600

    stubbed a few methods for the room, need backup

--- Project/Room.cs
+++ Project/Room.cs
@@ -8,5 +8,16 @@ namespace CastleGrimtol.Project
     public string Description { get; set; }
     public List<Item> Items { get; set; }
     public Dictionary<string, IRoom> Exits { get; set; }
+
+    //method for changing room if exit is an available choice (needs logic still)
+    public void changeRoom()
+    {
+
+    }
+    //method to take an item from a room (needs logic)
+    public void takeItem()
+    {
+
+    }
   }
 }
