408744c3a0c2f360721bd4e3c48662068be7d847
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-09-26 13:35:33 -0600

    testing new git repo

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using CastleGrimtol.Project;
-
+//testing
 namespace CastleGrimtol
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-
-        }
+      var game = new Game();
+      //  game.StartGame();
     }
+  }
 }
