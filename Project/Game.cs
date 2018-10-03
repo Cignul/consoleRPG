5e1726b631d6414564ebf7d43eebeb34ba63b4bb
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-10-03 14:34:11 -0600

    fixed almost everything, trying to get inventory printing correctly

--- Project/Game.cs
+++ Project/Game.cs
@@ -42,14 +42,19 @@ namespace CastleGrimtol.Project
       Console.WriteLine("What would you like to do?");
       string userInput = Console.ReadLine();
 
-      Console.WriteLine(CurrentRoom.Description);
+      //Console.WriteLine(CurrentRoom.Description);
     }
 
     public void Inventory()
     {
+      if (CurrentPlayer.Inventory.Contains(null))
+      {
+        Console.WriteLine("Your inventory is currently empty");
+      }
       Console.WriteLine(CurrentPlayer.Inventory);
-    }
+      Console.WriteLine($"{CurrentPlayer.Inventory.ToString()}");
 
+    }
     public void Look()
     {
       Console.WriteLine(CurrentRoom.Description);
@@ -72,15 +77,16 @@ namespace CastleGrimtol.Project
       var name = Console.ReadLine();
       Console.WriteLine();
       Item key = new Item("key", "a silver key");
-      _player = new Player(name);
+      CurrentPlayer = new Player(name);
       playing = true;
       //add a better description _player not coming in right..
       Room Room0 = new Room("Room0", $"Welcome to castle Grimtol {name}. Keep your head on a swivel. \n You find yourself in the entry plaza of the castle, a cold air eminates from within, there appears to be a door to the north");
-      Room Room1 = new Room("Room1", "this room looks treacherous, there appears to be a pit to the west, descending into darkness.  You faintly discern a door to the east");
+      Console.WriteLine(Room0.Description);
+      Room Room1 = new Room("Room1", "this room looks treacherous, there appears to be a pit to the west, descending into darkness.\n  You faintly discern a door to the east");
       Room Room2 = new Room("Room2", "You find yourself in a room where the walls and ceiling are completely covered in mirrored glass.\n  You notice rectangular cracks to the north that resemble the shape of a doorway");
-      Room Room3 = new Room("Room3", "You barely discern light ahead, , there is a locked door to the north. \n However, there appears to be a table with a key on it.  Someone wasn't paying attention to security, apparently.");
-      Room Room4 = new Room("Room4", "You see a passage that leads to daylight, your journey is complete. Congratulations!!");
-      Room Pit = new Room("Pit", "you fall about 20 feet and are impaled by pungee sticks, your journey ends here...");
+      Room Room3 = new Room("Room3", "You barely discern light ahead, an exit maybe?\n There is a locked door to the north. \n However, there appears to be a table with a key on it.  Someone wasn't paying attention to security, apparently.");
+      Room Room4 = new Room("Room4", "You see a passage that leads to daylight, your journey is complete.\n Congratulations!!");
+      Room Pit = new Room("Pit", "You fall about 20 feet and are impaled by pungee sticks.\n Your journey ends here...");
       CurrentRoom = Room0;
       //adding room exit paths
       Room0.Exits.Add("north", Room1);
@@ -99,13 +105,17 @@ namespace CastleGrimtol.Project
       while (playing)
       {
 
-        Console.WriteLine(CurrentRoom.Description);
+        //Console.WriteLine(CurrentRoom.Description);
         Console.WriteLine("Use <Help> for a list of commands");
         string userInput = Console.ReadLine().ToLower();
-        if (userInput == "help" || userInput == "HELP" || userInput == "Help")
+        if (userInput == "help")
         {
           Help();
           userInput = Console.ReadLine().ToLower();
+        }
+        if (userInput == "inventory")
+        {
+          Inventory();
         }
 
         if (userInput == "look")
@@ -143,6 +153,11 @@ namespace CastleGrimtol.Project
         {
           CurrentRoom = Room4;
           Console.WriteLine(CurrentRoom.Description);
+        }
+        if (userInput == "use key" && CurrentRoom == Room3)
+        {
+          Console.WriteLine(CurrentRoom.Description);
+          Console.WriteLine("The Door is Locked! You cannot pass");
         }
         if (userInput == "take key" && CurrentRoom == Room3)
         {
